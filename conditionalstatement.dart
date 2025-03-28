cca75a92cd23a6315724f71365fc5e01d270c627
“Nabin <pona_bca2079@lict.edu.np>
2025-03-28 11:07:42 +0545

    Some Update

---
+++ conditionalstatement.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  int a =10;
+  if (a>50)
+  {
+    print("The number is greater than 50");
+  }
+  else if
+  {
+    print("The number is less than 50");
+  }
+  else
+  {
+    print("The number is equal to 50");
+  }
+  String ab = "Nabin";
+  String b = "Poudel";
+  print("$ab $b");
+}
+
