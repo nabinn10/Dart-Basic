c2e5ec730aca48643647b9878ac7a5638fd2567a
“Nabin <pona_bca2079@lict.edu.np>
2025-03-31 21:57:43 +0545

    Reverso of number

---
+++ reverse.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  // reverse of number
+  int num = 12345;
+  int reverse = 0;
+  while (num != 0)
+  {
+    int digit = num % 10;
+    reverse = reverse * 10 + digit;
+    num ~/= 10; // integer division
+  }
+  print("The reverse of the number is $reverse");
+}
