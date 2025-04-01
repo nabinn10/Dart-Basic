f460d235aa64c23423eee4ee801570c3bd9cf415
“Nabin <pona_bca2079@lict.edu.np>
2025-04-01 13:52:32 +0545

    List update

--- listexample.dart
+++ listexample.dart
@@ -1,14 +1,19 @@
 void main()
 {
-  // take a list of 1 1 2 3 5 8 13 21 34 55 89
-  // print out al the elements of the list that are less than 5
   List<int> numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
   List<int> lessThanFive = [];
-  for (int number in numbers) {
-    if (number < 5) {
-      lessThanFive.add(number);
+  for (int i =0;i<numbers.length;i++)
+  {
+    if (numbers[i]<5)
+    {
+      lessThanFive.add(numbers[i]);
     }
   }
+  // for (int number in numbers) {
+  //   if (number < 5) {
+  //     lessThanFive.add(number);
+  //   }
+  // }
   print("Numbers less than 5: $lessThanFive");
-  
+
 }
